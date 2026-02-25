1334836fa39545fb054cb38ef71a839ce26c7f26
Jonviter Simbolon <jonviter17.developer@gmail.com>
2026-02-26 00:34:45 +0700

    Add Keyboard Avoiding

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -7,7 +7,17 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
 import { Controller, useForm } from "react-hook-form";
 
-import { Image, StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 import z from "zod";
 
 const signInScheme = z.object({
@@ -43,75 +53,86 @@ const LoginScreen = () => {
   };
 
   return (
-    <Container>
-      <View className="flex-1 items-center justify-center px-6 pt-16">
-        {/* Logo */}
-        {/* <View className="bg-white/20 p-4 rounded-2xl mb-6">
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <Container>
+        <View className="flex-1 items-center justify-center px-6 pt-16">
+          {/* Logo */}
+          {/* <View className="bg-white/20 p-4 rounded-2xl mb-6">
           <Feather name="hexagon" size={40} color="white" />
         </View> */}
 
-        <Text className="text-white text-3xl font-bold mb-2">
-          Selamat datang!
-        </Text>
+          <Text className="text-white text-3xl font-bold mb-2">
+            Selamat datang!
+          </Text>
 
-        <Text className="text-white/80 text-center text-base">
-          Halo reader,{"\n"}
-          Silahkan login ke aplikasi
-        </Text>
+          <Text className="text-white/80 text-center text-base">
+            Halo reader,{"\n"}
+            Silahkan login ke aplikasi
+          </Text>
 
-        {/* Illustration */}
-        <Image
-          source={{
-            uri: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
-          }}
-          className="w-60 h-60 mt-8"
-          resizeMode="contain"
+          {/* Illustration */}
+          <Image
+            source={{
+              uri: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+            }}
+            className="w-60 h-60 mt-8"
+            resizeMode="contain"
+          />
+        </View>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <ScrollView
+            contentContainerStyle={{ flexGrow: 1, justifyContent: "center" }}
+            keyboardShouldPersistTaps="handled"
+          >
+            <Controller
+              control={control}
+              name="username"
+              render={({ field: { onChange, value, onBlur } }) => (
+                <Input
+                  autoCapitalize="none"
+                  label="Username / Email"
+                  value={value}
+                  onChangeText={onChange}
+                  placeholder="Masukan username / email anda"
+                  error={errors.username ? errors.username.message : ""}
+                />
+              )}
+            />
+
+            <Controller
+              control={control}
+              name="password"
+              render={({ field: { onChange, value, onBlur } }) => (
+                <Input
+                  secureTextEntry={true}
+                  label="Pasword"
+                  value={value}
+                  onChangeText={onChange}
+                  placeholder="Masukan password anda"
+                  error={errors.password ? errors.password.message : ""}
+                />
+              )}
+            />
+          </ScrollView>
+        </TouchableWithoutFeedback>
+        <Button
+          title="Login"
+          disabled={isSubmitting}
+          onPress={handleSubmit(onSubmit)}
         />
-      </View>
-      <Controller
-        control={control}
-        name="username"
-        render={({ field: { onChange, value, onBlur } }) => (
-          <Input
-            autoCapitalize="none"
-            label="Username / Email"
-            value={value}
-            onChangeText={onChange}
-            placeholder="Masukan username / email anda"
-            error={errors.username ? errors.username.message : ""}
-          />
-        )}
-      />
 
-      <Controller
-        control={control}
-        name="password"
-        render={({ field: { onChange, value, onBlur } }) => (
-          <Input
-            secureTextEntry={true}
-            label="Pasword"
-            value={value}
-            onChangeText={onChange}
-            placeholder="Masukan password anda"
-            error={errors.password ? errors.password.message : ""}
-          />
-        )}
-      />
-
-      <Button
-        title="Login"
-        disabled={isSubmitting}
-        onPress={handleSubmit(onSubmit)}
-      />
-
-      <Button
-        title="Lanjutkan dengan Google"
-        disabled={isSubmitting}
-        onPress={signInWithGoogle}
-        icon="logo-google"
-      />
-      {/* Google Login Button */}
-    </Container>
+        <Button
+          title="Lanjutkan dengan Google"
+          disabled={isSubmitting}
+          onPress={signInWithGoogle}
+          icon="logo-google"
+        />
+        {/* Google Login Button */}
+      </Container>
+    </KeyboardAvoidingView>
   );
 };
 
